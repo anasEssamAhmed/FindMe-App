5ff0ae13bbdbadc4db6a5882cce871f856749888
Anas Ahmed <anasps201@gmail.com>
2024-12-24 11:58:22 +0200

    FindMe App

---
+++ app/src/main/java/com/example/findmeapp/login/Login.kt
@@ -0,0 +1,13 @@
+package com.example.findmeapp.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.findmeapp.R
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+    }
+}
---
+++ app/src/main/java/com/example/findmeapp/splash/Splash.kt
@@ -0,0 +1,21 @@
+package com.example.findmeapp.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.findmeapp.R
+import com.example.findmeapp.login.Login
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
